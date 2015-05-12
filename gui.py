e3f32d157e2be4e92240d5c3e0e446f208481fb7
Jacob <jroth@g.hmc.edu>
2015-05-11 20:37:13 -0500

    gui programming adventures have been sucessful! I have a simple GUI that can search for courses. Now it just needs to be finished.

--- gui.py
+++ gui.py
@@ -1,19 +1,42 @@
 from explore import *
 from tkinter import *
 
+
+class SearchUnit(Frame):
+    def __init__(self,master):
+        Frame.__init__(self,master)
+        self.scrollbar = Scrollbar(self, orient=VERTICAL)
+        self.listbox = Listbox(self, yscrollcommand=self.scrollbar.set)
+        self.searchtext = StringVar()
+        self.entry = Entry(self,textvariable=self.searchtext)
+        self.searchbutton = Button(self,text='Search')
+
+        self.scrollbar.config(command=self.listbox.yview)
+
+        self.entry.pack()
+        self.searchbutton.pack()
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.listbox.pack(side=LEFT, fill=BOTH, expand=1)
+
 master = Tk()
 
-frame = Frame(master)
-scrollbar = Scrollbar(frame, orient=VERTICAL)
-listbox = Listbox(frame, yscrollcommand=scrollbar.set)
-scrollbar.config(command=listbox.yview)
-scrollbar.pack(side=RIGHT, fill=Y)
-listbox.pack(side=LEFT, fill=BOTH, expand=1)
+searchunit = SearchUnit(master)
 
-addbutton = Button(master,text="Put Item In",command =lambda:  listbox.insert(END,"A"))
-clearbutton = Button(master,text="Clear Listbox",command =lambda:  listbox.delete(0,END))
+def fillLbWithSections(listbox, searchtext):
+    sectionCodes = [ sec.code for sec in sections if ( (str.lower(searchtext.get()) in str.lower(sec.code)) or (str.lower(searchtext.get()) in str.lower(sec.name)) ) ]
+    listbox.delete(0,END) # clear out the listbox
+    for code in sectionCodes:
+        listbox.insert(END,code)
 
-frame.pack()
-addbutton.pack()
-clearbutton.pack()
+
+searchunit.searchbutton.config(command= lambda: fillLbWithSections(searchunit.listbox, searchunit.searchtext))
+
+#addbutton = Button(master,text="Put Item In",command =lambda:  searchunit.listbox.insert(END,"A"))
+#clearbutton = Button(master,text="Clear Listbox",command =lambda:  searchunit.listbox.delete(0,END))
+#addbutton.pack()
+#clearbutton.pack()
+
+
+searchunit.pack()
+
 master.mainloop()
