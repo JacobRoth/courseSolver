d537ea5c35c2673bbe76711728cb5f901361f25e
Jacob <jroth@g.hmc.edu>
2015-05-17 14:57:04 -0500

    nobody's going to use this shitty TUI. I should get going on GUI's again.

--- tui.py
+++ tui.py
@@ -21,30 +21,49 @@ def printSchedule(schedule):
         print("") # pad space for next section
 
 def treeRender(blockStructure):
+    print(" ")
     for iii in range(len(blockStructure)):
         print("---Block ",iii)
+        if len(blockStructure[iii])== 0: #no sections here
+            print("   | No sections in this block")
         for jjj in range(len(blockStructure[iii])):
             print("   |-{1:3}: ".format(str(jjj)),blockStructure[iii][jjj])
+    print(" ")
 
 def tuiChoice(prompt,optionsList):
     '''loops until you make a valid choice from optionsList. Must be passing in a list of strings for optionsList'''
     assert(all(list(map( lambda x: type(x)==str, optionsList)))) # confirm withe the programmer that it's a list of all strings.
     received = input(prompt) 
     while ( not ( received.strip() in optionsList)):
+        print("Bad input, try again")
         received = input(prompt)
     return received.strip() # we use strip to take off any errant spaces.
 
 def main():
-    blockStructure = [] # reminder - a block structure is a list of lists of sections. course.schedules() takes in a block structure and returns the possible class shedules.
-    print("Welcome to the shitty text interface to Jacob's scheduling algorithm! Enter h for help")
+    blockStructure = [[]] # reminder - a block structure is a list of lists of sections. course.schedules() takes in a block structure and returns the possible class shedules.
+    print("Welcome to the mediocre text interface to Jacob's scheduling algorithm! Enter h for help")
     while (True):
-        cmd = tuiChoice("? ",['h','q','p'])
+        treeRender(blockStructure)
+        cmd = tuiChoice("? ",['h','q','ab','db','as','ds'])
         if cmd == 'h':
             print("All the help text goes here")
-        elif cmd == 'q':
+        elif cmd == 'q': # quit
             exit()
-        elif cmd == 'p':
-            treeRender(blockStructure)
+        elif cmd == 'ab': # add block
+            blockStructure.append([])
+        elif cmd == 'db': # delete block 
+            print("Enter the number of the block you wish to delete, or enter the word \"cancel\".")
+            blockToDelete = tuiChoice("Block number: ", list(map(str,range(len(blockStructure))))+["cancel"] )
+            if blockToDelete == 'cancel':
+                pass  # we aren't taking any action.
+            else:
+                del blockStructure[int(blockToDelete)] # python is sort of cool like this. We don't do something like myList.removeAtIndex(5), we just do del myList[7]. It's so clean.
 
+        elif cmd == 'as': # add section
+            pass # have to program in the search function
+        else:
+            print("Somehow we hit an unhandled case in the main loop cmd branches! Check your tuiChoice code, programmer!")
+            exit()
+        
 if __name__ == '__main__':
     main()
